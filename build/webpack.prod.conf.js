d2dc7c19c46b2a092bb66f80dd7aeaab33b315fa
quietcoder <1642965215@qq.com>
2017-05-04 15:27:43 +0800

    新增：github展示页面

--- build/webpack.prod.conf.js
+++ build/webpack.prod.conf.js
@@ -8,9 +8,10 @@ var ExtractTextPlugin = require('extract-text-webpack-plugin')
 module.exports = merge(baseWebpackConfig, {
   devtool: '#source-map',
   output: {
-    path: path.resolve(__dirname, '../dist'),
+    path: path.resolve(__dirname, '../docs'),
     filename: 'js/[name].[chunkhash:8].js',
-    chunkFilename: 'js/[id].[chunkhash:8].js'
+    chunkFilename: 'js/[id].[chunkhash:8].js',
+    publicPath: '/checkbox-tree'
   },
   plugins: [
     new webpack.DefinePlugin({
